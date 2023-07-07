9315c8b089ee16bcc94a075ae5258bf137e8e09f
Josh Bennett <joshbennett793@yahoo.com>
2023-07-06 21:00:47 -0500

    Remove array syntax from test

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,14 +8,12 @@ describe('starting a game', () => {
   it('should create a list of cards from a dataset', () => {
     const cards = createCardListFromDataset(sampleData);
 
-    expect(cards[0]).to.deep.equal([
-      {
-        id: 1,
-        question:
-          'What allows you to define a set of related information using key-value pairs?',
-        answers: ['object', 'array', 'function'],
-        correctAnswer: 'object',
-      },
-    ]);
+    expect(cards[0]).to.deep.equal({
+      id: 1,
+      question:
+        'What allows you to define a set of related information using key-value pairs?',
+      answers: ['object', 'array', 'function'],
+      correctAnswer: 'object',
+    });
   });
 });
